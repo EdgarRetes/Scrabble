58f96ced1bd32b5aa01ec20949e712e9ac557eec
Edgar Martínez Retes <113946434+EdgarRetes@users.noreply.github.com>
2023-11-11 15:20:43 -0600

    Add files via upload

---
+++ scrabble.c
@@ -0,0 +1,56 @@
+#include <ctype.h>
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+// Points assigned to each letter of the alphabet
+int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
+
+int compute_score(string word);
+
+int main(void)
+{
+    // Get input words from both players
+    string word1 = get_string("Player 1: ");
+    string word2 = get_string("Player 2: ");
+
+    // Score both words
+    int score1 = compute_score(word1);
+    int score2 = compute_score(word2);
+
+    // TODO: Print the winner
+    if (score1 > score2)
+    {
+        printf("Player 1 wins!\n");
+    }
+    else if (score1 < score2)
+    {
+        printf("Player 2 wins!\n");
+    }
+    else
+    {
+        printf("Tie!\n");
+    }
+}
+
+int compute_score(string word)
+{
+    // TODO: Compute and return score for string
+
+    int n = 0; //initialize position in list of points
+    int sum = 0;
+    for (int i = 0; i < strlen(word); i++)
+    {
+        int value = toupper(word[i]); //the code neglects upper and lower cases so everything converts to upper
+        int position = value - 65;
+        if (isalpha(word[i]))
+        {
+            sum += POINTS[position];
+        }
+        else
+        {
+            sum += 0;
+        }
+    }
+    return sum;
+}
